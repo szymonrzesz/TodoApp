1a90fd28152f0b690e008882e7b5d3687149e771
Szymon Rzeszotarski <shymon.rzesz@gmail.com>
2025-06-29 12:25:13 +0200

    Adding Api .NET part of Project

---
+++ WebApi/Models/EventDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class EventDto
+    {
+        [Required]
+        public string Title { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        [Required]
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public bool AllDay { get; set; }
+    }
+}
---
+++ WebApi/Services/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+namespace WebApi.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public required DbSet<Event> Events { get; set; }
+    }
+}
